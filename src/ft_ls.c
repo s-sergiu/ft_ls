88968ab1cf4c72799e146960f9008c1e701bbaed
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-11-24 17:25:21 +0100

    using ft_printf

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdio.h>
 #include "../libs/Libft/include/libft.h"
 
 int main(int argc, char **argv, char **envp) {
@@ -8,7 +7,7 @@ int main(int argc, char **argv, char **envp) {
 	(void)envp;
 	int length = ft_strlen("test");
 	write(1, "Success\n", 8);
-	printf("length %d\n", length);
+	ft_printf("length %d\n", length);
 
 	return (0);
 }
