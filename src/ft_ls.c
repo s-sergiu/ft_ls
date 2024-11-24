4ff8ff73b26cd96185814f73b17a4ddb898a3304
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-11-24 21:00:54 +0100

    implemented get_pwd

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -1,12 +1,23 @@
 #include <unistd.h>
 #include "libft.h"
 
-int main(int argc, char **argv, char **envp) {
-	(void)argc;
-	(void)argv;
-	(void)envp;
+char *get_pwd(char **envp) {
+	int i = 0;
 
-	ft_printf("%s\n", envp[7]);
+	while (envp[++i] != NULL)
+		if (!ft_strncmp(envp[i], "PWD", 3))
+			return envp[i] + 4;
+	return (NULL);
+}
+
+int main(int argc, char **argv, char **envp) {
+	char *pwd;
+
+	if (argc > 1)
+		ft_printf("args: %s\n", *argv);
+
+	pwd = get_pwd(envp);
+	ft_printf("%s\n", pwd);
 
 	return (0);
 }
