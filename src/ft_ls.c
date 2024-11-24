b575dfc7165fa7c124ade6c85be5986ea39e6c06
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-11-24 20:15:31 +0100

    update

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -5,9 +5,8 @@ int main(int argc, char **argv, char **envp) {
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	int length = ft_strlen("test");
-	write(1, "Success\n", 8);
-	ft_printf("length %d\n", length);
+
+	ft_printf("%s\n", envp[7]);
 
 	return (0);
 }
