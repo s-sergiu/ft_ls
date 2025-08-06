441f0d9673018071abce983810b2f116c2482f74
Sergiu Ster <ssergiu@student.42heilbronn.de>
2025-08-06 08:21:37 +0200

    Added flag struct     *added useful info at the bottom of header file

--- include/ft_ls.h
+++ include/ft_ls.h
@@ -17,16 +17,47 @@
 #define FLAGS 1
 #define NAME 2
 #define ILLEGAL_FLAG 10
+#define ACCESS_FLAG 11
 #define FLAG_HIDDEN 0x1
 #define FLAG_VISIBLE 0x2
 
+typedef struct s_flags t_flags;
+
+struct s_flags {
+	int	exit_status;
+	int valid_dirs;
+};
+
 int				is_output_a_terminal();
-void			execute(char**);
-void			scan_directory(char[][NAME_MAXLEN], char*);
+t_flags			*init_flags(void);
+void			execute(const char**);
+void			scan_directory(char[][NAME_MAXLEN], const char*);
 void			print_debug_info(void);
-int				get_number_of_files(char*);
-int				handle_args(char**);
-void			check_illegal_flag(char*);
-void			send_error(int, char);
+int				get_number_of_files(const char*);
+int				handle_args(const char**, int);
+int				is_valid_flag(const char**, int);
+void			send_error(int, const char**, int);
 
 #endif
+
+/* 
+ * Allowed functions:
+ * write();
+ * opendir();
+ * readdir();
+ * closedir();
+ * stat();
+ * lstat();
+ * getpwuid();
+ * getgrgid();
+ * listxattr();
+ * getxattr();
+ * time();
+ * ctime();
+ * readlink();
+ * malloc();
+ * free();
+ * perror();
+ * strerror();
+ * exit();
+*/
